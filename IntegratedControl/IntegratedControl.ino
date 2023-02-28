856a365e870838cea2ba4c502c334c457ab604d6
AdamCassie <adam.cassie@mail.utoronto.ca>
2023-02-28 16:13:34 -0500

    Removed optimization appraoch

--- IntegratedControl/IntegratedControl.ino
+++ IntegratedControl/IntegratedControl.ino
@@ -8,7 +8,7 @@
 #define DE 7
 
 // Pump burst times
-#define MOISTURE_DELAY 500
+#define MOISTURE_DELAY 5000
 #define N_DELAY 500
 #define P_DELAY 500
 #define K_DELAY 500
@@ -36,7 +36,6 @@ typedef struct param
 {
   int value;
   int target;
-  int gradient;
   int delay;
 } Param;
 
@@ -53,9 +52,9 @@ typedef struct rank
 
 // Global control variables
 Param moisture = {0, 0, MOISTURE_DELAY};
-Param n = {0, 0, 0, N_DELAY};
-Param p = {0, 0, 0, P_DELAY};
-Param k = {0, 0, 0, K_DELAY};
+Param n = {0, 0, N_DELAY};
+Param p = {0, 0, P_DELAY};
+Param k = {0, 0, K_DELAY};
 
 // Global variable for priority of each plant nutrient
 Rank nutrient_priority = {'N', 'P', 'K', NULL, NULL, NULL};
@@ -486,25 +485,25 @@ int compute_modal_moisture(int a[], int n)
   return result;
 }
 
-// Loss function for gradient descent algorithm
-int compute_loss()
-{
-  int moisture_offset = moisture.target - moisture.value;
-  int n_offset = n.target - n.value;
-  int p_offset = p.target - p.value;
-  int k_offset = k.target - k.value;
-  int result = (moisture_offset ^ 2) + (n_offset ^ 2) + (p_offset ^ 2) + (k_offset ^ 2);
-  return result;
-}
+// // Loss function for gradient descent algorithm
+// int compute_loss()
+// {
+//   int moisture_offset = moisture.target - moisture.value;
+//   int n_offset = n.target - n.value;
+//   int p_offset = p.target - p.value;
+//   int k_offset = k.target - k.value;
+//   int result = (moisture_offset ^ 2) + (n_offset ^ 2) + (p_offset ^ 2) + (k_offset ^ 2);
+//   return result;
+// }
 
-void compute_gradient()
-{
-  int moisture_offset = moisture.target - moisture.value;
-  int n_offset = n.target - n.value;
-  int p_offset = p.target - p.value;
-  int k_offset = k.target - k.value;
-  moisture.gradient = 2 * moisture.offset;
-  n.gradient = 2 * n.offset;
-  p.gradient = 2 * p.offset;
-  k.gradient = 2 * k.offset;
-}
+// void compute_gradient()
+// {
+//   int moisture_offset = moisture.target - moisture.value;
+//   int n_offset = n.target - n.value;
+//   int p_offset = p.target - p.value;
+//   int k_offset = k.target - k.value;
+//   moisture.gradient = 2 * moisture.offset;
+//   n.gradient = 2 * n.offset;
+//   p.gradient = 2 * p.offset;
+//   k.gradient = 2 * k.offset;
+// }
