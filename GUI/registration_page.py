46c815de62ab0e7662f32956f7015cf71b15dcdf
lallapalooza91 <samuellalla@live.com>
2023-03-25 17:21:21 -0400

    Registration page first commit. Blank page. Pushed so that we could link to it from other pages.

---
+++ GUI/registration_page.py
@@ -0,0 +1,18 @@
+import PySimpleGUI as sg
+
+# Create the PySimpleGUI layout
+layout = [
+    [sg.Button('Close')]
+]
+
+# Create the PySimpleGUI window
+window = sg.Window('Registration', layout, size=(1500,1500),location=(0,0),finalize=True)
+
+# Run the PySimpleGUI event loop
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event == 'Close':
+        break
+
+# Close the window
+window.close()
