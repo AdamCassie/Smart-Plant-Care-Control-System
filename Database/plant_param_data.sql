ae1c978e7020082cad2c31c87f4f18d029d98095
AdamCassie <adam.cassie@mail.utoronto.ca>
2023-04-05 21:23:20 -0400

    Added values to db

--- Database/plant_param_data.sql
+++ Database/plant_param_data.sql
@@ -2,4 +2,33 @@
 insert into IdealPlantParams values ('Plant1', 60, 20, 20, 20);
 insert into IdealPlantParams values ('Plant2', 60, 30, 30, 30);
 insert into IdealPlantParams values ('Plant3', 60, 40, 40, 40);
-
+-- Actual moisture values. 
+-- Defaulted to 15% moisture for dry plants.
+-- Used the minimum of the moisture range for non-dry plants.
+-- Placeholders for NPK
+insert into IdealPlantParams values ('Spider Plant', 32, 40, 40, 40);
+insert into IdealPlantParams values ('Norfolk Island Plant', 32, 40, 40, 40);
+insert into IdealPlantParams values ('Peace Lily', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Snake Plant', 15, 40, 40, 40);
+insert into IdealPlantParams values ('Chinese Money Plant', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Arrowhead Plant', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Corn Plant', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Jade Plant', 15, 40, 40, 40);
+insert into IdealPlantParams values ('Cast Iron Plant', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Pothos', 15, 40, 40, 40);
+insert into IdealPlantParams values ('English Ivy', 15, 40, 40, 40);
+insert into IdealPlantParams values ('Moth Orchid', 15, 40, 40, 40);
+insert into IdealPlantParams values ('Calathea', 42, 40, 40, 40);
+insert into IdealPlantParams values ('Rubber Tree', 32, 40, 40, 40);
+insert into IdealPlantParams values ('ZZ Plant', 15, 40, 40, 40);
+insert into IdealPlantParams values ('Dumb Canes', 32, 40, 40, 40);
+-- Placeholders for both moisture and NPK
+insert into IdealPlantParams values ('Heart-Leaf Philodendron', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Wax Plant', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Australia Umbrella Tree', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Fiddle-Leaf Fig', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Goldfish Plant', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Weeping Fig', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Widow''s Thrill', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Swiss Cheese Plant', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Ponytail Palm Plant', 67, 40, 40, 40);
