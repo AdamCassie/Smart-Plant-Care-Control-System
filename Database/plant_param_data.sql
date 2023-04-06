c17c37589194a5a272be9183aaddd2e9b7cce9d7
AdamCassie <adam.cassie@mail.utoronto.ca>
2023-04-05 20:03:23 -0400

    Update plant_param_data.sql

--- Database/plant_param_data.sql
+++ Database/plant_param_data.sql
@@ -1,5 +1,5 @@
 -- a few test values
-insert into IdealPlantParams values ('Plant1', 20, 20, 20, 20);
-insert into IdealPlantParams values ('Plant2', 30, 30, 30, 30);
-insert into IdealPlantParams values ('Plant3', 40, 40, 40, 40);
+insert into IdealPlantParams values ('Plant1', 60, 20, 20, 20);
+insert into IdealPlantParams values ('Plant2', 60, 30, 30, 30);
+insert into IdealPlantParams values ('Plant3', 60, 40, 40, 40);
 
