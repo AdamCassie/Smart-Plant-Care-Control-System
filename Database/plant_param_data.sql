15023e6642db187fb1b043b09992a497451245c5
JerushaDrugah <Jerushadurgah97@gmail.com>
2023-04-06 02:42:16 -0400

    updated database values

--- Database/plant_param_data.sql
+++ Database/plant_param_data.sql
@@ -1,34 +1,28 @@
 -- a few test values
-insert into IdealPlantParams values ('Plant1', 60, 20, 20, 20);
-insert into IdealPlantParams values ('Plant2', 60, 30, 30, 30);
-insert into IdealPlantParams values ('Plant3', 60, 40, 40, 40);
--- Actual moisture values. 
--- Defaulted to 15% moisture for dry plants.
--- Used the minimum of the moisture range for non-dry plants.
--- Placeholders for NPK
-insert into IdealPlantParams values ('Spider Plant', 32, 40, 40, 40);
-insert into IdealPlantParams values ('Norfolk Island Plant', 32, 40, 40, 40);
-insert into IdealPlantParams values ('Peace Lily', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Snake Plant', 15, 40, 40, 40);
-insert into IdealPlantParams values ('Chinese Money Plant', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Arrowhead Plant', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Corn Plant', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Jade Plant', 15, 40, 40, 40);
-insert into IdealPlantParams values ('Cast Iron Plant', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Pothos', 15, 40, 40, 40);
-insert into IdealPlantParams values ('English Ivy', 15, 40, 40, 40);
-insert into IdealPlantParams values ('Moth Orchid', 15, 40, 40, 40);
-insert into IdealPlantParams values ('Calathea', 42, 40, 40, 40);
-insert into IdealPlantParams values ('Rubber Tree', 32, 40, 40, 40);
-insert into IdealPlantParams values ('ZZ Plant', 15, 40, 40, 40);
-insert into IdealPlantParams values ('Dumb Canes', 32, 40, 40, 40);
--- Placeholders for both moisture and NPK
-insert into IdealPlantParams values ('Heart-Leaf Philodendron', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Wax Plant', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Australia Umbrella Tree', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Fiddle-Leaf Fig', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Goldfish Plant', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Weeping Fig', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Widow''s Thrill', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Swiss Cheese Plant', 67, 40, 40, 40);
-insert into IdealPlantParams values ('Ponytail Palm Plant', 67, 40, 40, 40);
+insert into IdealPlantParams values ('Spider Plant', 32, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Norfolk Island Pine', 32, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Peace Lily', 42, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Snake Plant', 31, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Chinese Money Plant', 50, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Arrowhead Plant', 50, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Corn Plant', 50, 10000, 10000, 10000);
+insert into IdealPlantParams values ('Jade Plant', 31, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Cast Iron Plant', 50, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Pothos', 31, 100000, 100000, 100000);
+insert into IdealPlantParams values ('English Ivy', 31, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Moth Orchid', 31, 300000, 100000, 100000);
+insert into IdealPlantParams values ('Calathea', 42, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Rubber Tree', 32, 100000, 100000, 100000);
+insert into IdealPlantParams values ('ZZ Plant', 31, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Dumb Canes', 32, 100000, 100000, 100000);
+insert into IdealPlantParams values ('Heart-Leaf Philodendron', 32, 200000, 200000, 200000);
+insert into IdealPlantParams values ('Wax Plant', 42, 20000, 10000, 20000);
+insert into IdealPlantParams values ('Australia Umbrella Tree', 42, 80000, 80000, 80000);
+insert into IdealPlantParams values ('Fiddle-Leaf Fig', 42, 30000, 20000, 10000);
+insert into IdealPlantParams values ('Goldfish Plant', 32, 10000, 30000, 10000);
+insert into IdealPlantParams values ('Weeping Fig', 42, 80000, 80000, 80000);
+insert into IdealPlantParams values ('Golden Pothos', 42, 30000, 20000, 10000);
+insert into IdealPlantParams values ('Swiss Cheese Plant', 32, 50000, 20000, 30000);
+insert into IdealPlantParams values ('Ponytail Palm Plant', 32, 2500, 1300, 1900);
+
+
